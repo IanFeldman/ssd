8312c52fc0bec6d615127db64fbaa67a480efb75
Ian Feldman <ianrfeldman@gmail.com>
2025-02-04 23:46:46 -0800

    Create print function

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <8051.h>
+#include <string.h>
 
 void delay()
 {
@@ -12,29 +13,40 @@ void delay()
 void init_uart()
 {
     /* timer */
-    TMOD  = 0x20;   /* timer 1 in mode 2 (8-bit overflow) */
-    TH1   = 0xF5;   /* timer reload value */
-    PCON |= 0x80;
-    TCON |= 0x40;
+    TMOD |= 0x20;   /* timer 1 in mode 2 (8-bit overflow) */
+    TH1   = 0xF5;   /* timer reload value for baud = 9600 */
+    PCON |= 0x80;   /* set SMOD1 */
+    TCON |= 0x40;   /* enable timer */
 
     /* uart */
     SCON  = 0x50;    /* mode 1 (8 bit tx/rx) */
 }
 
 
+void print(char *string)
+{
+    const char *str = string;
+    if (!string)
+    {
+        str = "Error string is null";
+    }
+    for (int i = 0; i < strlen(str); i++)
+    {
+        SBUF = str[i];
+        while (TI == 0);
+        TI = 0;
+    }
+}
+
+
 void main()
 {
-    /* setup UART */
     init_uart();
     P1 = 0x00;
     while(1)
     {
         P1 ^= 0x01;
-        SBUF = 'a';
-
-        while (TI == 0);
-        TI = 0;
-
+        print("Hello, world! ");
         delay();
     }   
 }
