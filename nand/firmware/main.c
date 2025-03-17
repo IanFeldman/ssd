fcd930d865ebac02c15708a5146a3f5a13b240af
Ian Feldman <ianrfeldman@gmail.com>
2025-03-16 23:40:16 -0700

    Configure uart for hello world

--- nand/firmware/main.c
+++ nand/firmware/main.c
@@ -5,14 +5,20 @@
 
 int main()
 {
+    /* configure uart */
     uart_init();
+    uart_print_esc(CLEAR_SCREEN);
+    uart_print_esc(HOME_CURSOR);
+
+    /* set pin as output */
     DDRB |= (1 << PB0);
 
     while (1)
     {
         PORTB |= (1 << PB0);
         _delay_ms(250);
-        uart_print("test");
+        uart_print("Hello, world.");
+        uart_print_esc(NEW_LINE);
 
         PORTB &= ~(1 << PB0);
         _delay_ms(250);
