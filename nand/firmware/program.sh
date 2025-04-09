20c78030ff04c860c323039b73b7e014a0a16ac4
IanFeldman <ianrfeldman@gmail.com>
2025-04-09 13:58:16 -0700

    Update program scripts

--- nand/firmware/program.sh
+++ nand/firmware/program.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-avrdude -c stk500v1 -P /dev/ttyACM0 -p m32u2 -b 19200 -U flash:w:$1
+avrdude -c stk500v1 -P /dev/ttyACM0 -p m32u2 -b 19200 -U flash:w:nand.hex
 
--- nor/firmware/program.sh
+++ nor/firmware/program.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-avrdude -C ../hardware/AT89S5x.conf -c stk500v1 -P /dev/ttyACM0 -p 89s51 -b 19200 -U flash:w:$1
+avrdude -C ../hardware/AT89S5x.conf -c stk500v1 -P /dev/ttyACM0 -p 89s51 -b 19200 -U flash:w:nor.hex
 
