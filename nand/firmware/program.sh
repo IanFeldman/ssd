7e196365fb3be7dd7d0a4801c36384350863dbb9
IanFeldman <ianrfeldman@gmail.com>
2025-05-14 14:56:53 -0700

    Create test branch

--- nand/firmware/program.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-avrdude -c stk500v1 -P /dev/ttyACM0 -p m32u2 -b 19200 -U flash:w:nand.hex
-
--- nand/firmware/set-fuses.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-avrdude -c stk500v1 -p m32u2 -P /dev/ttyACM0 -b 19200 -U lfuse:w:0xFF:m -U hfuse:w:0xD7:m
-
