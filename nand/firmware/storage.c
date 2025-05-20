19bc9314931c5f3e1d49102420758cc9d3f7275b
IanFeldman <ianrfeldman@gmail.com>
2025-05-20 11:35:31 -0700

    Convert tabs to spaces

--- nand/firmware/storage.c
+++ nand/firmware/storage.c
@@ -44,25 +44,25 @@
  *  within a device can be differentiated from one another.
  */
 USB_ClassInfo_MS_Device_t Disk_MS_Interface =
-	{
-		.Config =
-			{
-				.InterfaceNumber           = INTERFACE_ID_MassStorage,
-				.DataINEndpoint            =
-					{
-						.Address           = MASS_STORAGE_IN_EPADDR,
-						.Size              = MASS_STORAGE_IO_EPSIZE,
-						.Banks             = 1,
-					},
-				.DataOUTEndpoint           =
-					{
-						.Address           = MASS_STORAGE_OUT_EPADDR,
-						.Size              = MASS_STORAGE_IO_EPSIZE,
-						.Banks             = 1,
-					},
-				.TotalLUNs                 = TOTAL_LUNS,
-			},
-	};
+    {
+        .Config =
+            {
+                .InterfaceNumber           = INTERFACE_ID_MassStorage,
+                .DataINEndpoint            =
+                    {
+                        .Address           = MASS_STORAGE_IN_EPADDR,
+                        .Size              = MASS_STORAGE_IO_EPSIZE,
+                        .Banks             = 1,
+                    },
+                .DataOUTEndpoint           =
+                    {
+                        .Address           = MASS_STORAGE_OUT_EPADDR,
+                        .Size              = MASS_STORAGE_IO_EPSIZE,
+                        .Banks             = 1,
+                    },
+                .TotalLUNs                 = TOTAL_LUNS,
+            },
+    };
 
 
 /** Main program entry point. This routine contains the overall program flow, including initial
@@ -70,89 +70,89 @@ USB_ClassInfo_MS_Device_t Disk_MS_Interface =
  */
 int main(void)
 {
-	SetupHardware();
+    SetupHardware();
 
-	GlobalInterruptEnable();
+    GlobalInterruptEnable();
 
-	for (;;)
-	{
-		MS_Device_USBTask(&Disk_MS_Interface);
-		USB_USBTask();
-	}
+    for (;;)
+    {
+        MS_Device_USBTask(&Disk_MS_Interface);
+        USB_USBTask();
+    }
 }
 
 /** Configures the board hardware and chip peripherals for the demo's functionality. */
 void SetupHardware(void)
 {
 #if (ARCH == ARCH_AVR8)
-	/* Disable watchdog if enabled by bootloader/fuses */
-	MCUSR &= ~(1 << WDRF);
-	wdt_disable();
+    /* Disable watchdog if enabled by bootloader/fuses */
+    MCUSR &= ~(1 << WDRF);
+    wdt_disable();
 
-	/* Disable clock division */
-	clock_prescale_set(clock_div_1);
+    /* Disable clock division */
+    clock_prescale_set(clock_div_1);
 #elif (ARCH == ARCH_XMEGA)
-	/* Start the PLL to multiply the 2MHz RC oscillator to 32MHz and switch the CPU core to run from it */
-	XMEGACLK_StartPLL(CLOCK_SRC_INT_RC2MHZ, 2000000, F_CPU);
-	XMEGACLK_SetCPUClockSource(CLOCK_SRC_PLL);
+    /* Start the PLL to multiply the 2MHz RC oscillator to 32MHz and switch the CPU core to run from it */
+    XMEGACLK_StartPLL(CLOCK_SRC_INT_RC2MHZ, 2000000, F_CPU);
+    XMEGACLK_SetCPUClockSource(CLOCK_SRC_PLL);
 
-	/* Start the 32MHz internal RC oscillator and start the DFLL to increase it to 48MHz using the USB SOF as a reference */
-	XMEGACLK_StartInternalOscillator(CLOCK_SRC_INT_RC32MHZ);
-	XMEGACLK_StartDFLL(CLOCK_SRC_INT_RC32MHZ, DFLL_REF_INT_USBSOF, F_USB);
+    /* Start the 32MHz internal RC oscillator and start the DFLL to increase it to 48MHz using the USB SOF as a reference */
+    XMEGACLK_StartInternalOscillator(CLOCK_SRC_INT_RC32MHZ);
+    XMEGACLK_StartDFLL(CLOCK_SRC_INT_RC32MHZ, DFLL_REF_INT_USBSOF, F_USB);
 
-	PMIC.CTRL = PMIC_LOLVLEN_bm | PMIC_MEDLVLEN_bm | PMIC_HILVLEN_bm;
+    PMIC.CTRL = PMIC_LOLVLEN_bm | PMIC_MEDLVLEN_bm | PMIC_HILVLEN_bm;
 #endif
 
-	/* Hardware Initialization */
-	uart_init();
+    /* Hardware Initialization */
+    uart_init();
         flash_init();
-	Dataflash_Init();
-	USB_Init();
+    Dataflash_Init();
+    USB_Init();
 
         /* Clear uart terminal */
-	uart_print_esc(CLEAR_SCREEN);
-	uart_print_esc(HOME_CURSOR);
+    uart_print_esc(CLEAR_SCREEN);
+    uart_print_esc(HOME_CURSOR);
 
         /* test flash */
         test_all();
 
-	/* Check if the Dataflash is working, abort if not */
-	if (!(DataflashManager_CheckDataflashOperation()))
-	{
-		uart_print_ln("Dataflash operation failure");
-		for(;;);
-	}
+    /* Check if the Dataflash is working, abort if not */
+    if (!(DataflashManager_CheckDataflashOperation()))
+    {
+        uart_print_ln("Dataflash operation failure");
+        for(;;);
+    }
 
-	/* Clear Dataflash sector protections, if enabled */
-	DataflashManager_ResetDataflashProtections();
+    /* Clear Dataflash sector protections, if enabled */
+    DataflashManager_ResetDataflashProtections();
 }
 
 /** Event handler for the library USB Connection event. */
 void EVENT_USB_Device_Connect(void)
 {
-	uart_print_ln("USB connection event");
+    uart_print_ln("USB connection event");
 }
 
 /** Event handler for the library USB Disconnection event. */
 void EVENT_USB_Device_Disconnect(void)
 {
-	uart_print_ln("USB disconnection event");
+    uart_print_ln("USB disconnection event");
 }
 
 /** Event handler for the library USB Configuration Changed event. */
 void EVENT_USB_Device_ConfigurationChanged(void)
 {
-	bool ConfigSuccess = true;
+    bool ConfigSuccess = true;
 
-	ConfigSuccess &= MS_Device_ConfigureEndpoints(&Disk_MS_Interface);
+    ConfigSuccess &= MS_Device_ConfigureEndpoints(&Disk_MS_Interface);
 
-	uart_print_ln("USB connection changed event");
+    uart_print_ln("USB connection changed event");
 }
 
 /** Event handler for the library USB Control Request reception event. */
 void EVENT_USB_Device_ControlRequest(void)
 {
-	MS_Device_ProcessControlRequest(&Disk_MS_Interface);
+    MS_Device_ProcessControlRequest(&Disk_MS_Interface);
 }
 
 /** Mass Storage class driver callback function the reception of SCSI commands from the host, which must be processed.
@@ -161,12 +161,12 @@ void EVENT_USB_Device_ControlRequest(void)
  */
 bool CALLBACK_MS_Device_SCSICommandReceived(USB_ClassInfo_MS_Device_t* const MSInterfaceInfo)
 {
-	bool CommandSuccess;
+    bool CommandSuccess;
 
-	uart_print_ln("USB received SCSI command");
-	CommandSuccess = SCSI_DecodeSCSICommand(MSInterfaceInfo);
-	uart_print_ln("USB handled SCSI command");
+    uart_print_ln("USB received SCSI command");
+    CommandSuccess = SCSI_DecodeSCSICommand(MSInterfaceInfo);
+    uart_print_ln("USB handled SCSI command");
 
-	return CommandSuccess;
+    return CommandSuccess;
 }
 
