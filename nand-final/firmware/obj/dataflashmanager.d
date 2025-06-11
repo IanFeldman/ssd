bd5074a07e30c0ed13a37094a36b83093e79dc58
IanFeldman <ianrfeldman@gmail.com>
2025-06-11 14:00:56 -0700

    Rename folders

---
+++ nand-final/firmware/obj/dataflashmanager.d
@@ -0,0 +1,313 @@
+obj/dataflashmanager.o: lib/dataflashmanager.c lib/dataflashmanager.h \
+ lib/../storage.h lib/../descriptors.h third_party/LUFA/Drivers/USB/USB.h \
+ third_party/LUFA/Drivers/USB/../../Common/Common.h \
+ third_party/LUFA/Drivers/USB/../../Common/Architectures.h \
+ third_party/LUFA/Drivers/USB/../../Common/BoardTypes.h \
+ third_party/LUFA/Drivers/USB/../../Common/ArchitectureSpecific.h \
+ third_party/LUFA/Drivers/USB/../../Common/CompilerSpecific.h \
+ third_party/LUFA/Drivers/USB/../../Common/Attributes.h \
+ config/lufaconfig.h \
+ third_party/LUFA/Drivers/USB/../../Common/Endianness.h \
+ third_party/LUFA/Drivers/USB/Core/USBMode.h \
+ third_party/LUFA/Drivers/USB/Core/../../../Common/Common.h \
+ third_party/LUFA/Drivers/USB/Core/USBTask.h \
+ third_party/LUFA/Drivers/USB/Core/USBMode.h \
+ third_party/LUFA/Drivers/USB/Core/USBController.h \
+ third_party/LUFA/Drivers/USB/Core/AVR8/USBController_AVR8.h \
+ third_party/LUFA/Drivers/USB/Core/AVR8/../../../../Common/Common.h \
+ third_party/LUFA/Drivers/USB/Core/AVR8/../USBMode.h \
+ third_party/LUFA/Drivers/USB/Core/AVR8/../Events.h \
+ third_party/LUFA/Drivers/USB/Core/AVR8/../../../../Common/Common.h \
+ third_party/LUFA/Drivers/USB/Core/AVR8/../USBMode.h \
+ third_party/LUFA/Drivers/USB/Core/AVR8/../USBTask.h \
+ third_party/LUFA/Drivers/USB/Core/AVR8/../USBInterrupt.h \
+ third_party/LUFA/Drivers/USB/Core/AVR8/../AVR8/USBInterrupt_AVR8.h \
+ third_party/LUFA/Drivers/USB/Core/AVR8/../AVR8/../../../../Common/Common.h \
+ third_party/LUFA/Drivers/USB/Core/AVR8/../AVR8/../USBMode.h \
+ third_party/LUFA/Drivers/USB/Core/AVR8/../AVR8/../Events.h \
+ third_party/LUFA/Drivers/USB/Core/AVR8/../AVR8/../USBController.h \
+ third_party/LUFA/Drivers/USB/Core/AVR8/../Device.h \
+ third_party/LUFA/Drivers/USB/Core/AVR8/../StdDescriptors.h \
+ third_party/LUFA/Drivers/USB/Core/AVR8/../Events.h \
+ third_party/LUFA/Drivers/USB/Core/AVR8/../USBInterrupt.h \
+ third_party/LUFA/Drivers/USB/Core/AVR8/../Endpoint.h \
+ third_party/LUFA/Drivers/USB/Core/AVR8/../AVR8/Endpoint_AVR8.h \
+ third_party/LUFA/Drivers/USB/Core/AVR8/../AVR8/../USBTask.h \
+ third_party/LUFA/Drivers/USB/Core/AVR8/../AVR8/../USBInterrupt.h \
+ third_party/LUFA/Drivers/USB/Core/AVR8/../AVR8/Device_AVR8.h \
+ third_party/LUFA/Drivers/USB/Core/AVR8/../AVR8/../StdDescriptors.h \
+ third_party/LUFA/Drivers/USB/Core/AVR8/../AVR8/../Endpoint.h \
+ third_party/LUFA/Drivers/USB/Core/AVR8/../Endpoint.h \
+ third_party/LUFA/Drivers/USB/Core/AVR8/../DeviceStandardReq.h \
+ third_party/LUFA/Drivers/USB/Core/AVR8/../StdRequestType.h \
+ third_party/LUFA/Drivers/USB/Core/AVR8/../USBTask.h \
+ third_party/LUFA/Drivers/USB/Core/AVR8/../USBController.h \
+ third_party/LUFA/Drivers/USB/Core/AVR8/../EndpointStream.h \
+ third_party/LUFA/Drivers/USB/Core/AVR8/../AVR8/EndpointStream_AVR8.h \
+ third_party/LUFA/Drivers/USB/Core/Events.h \
+ third_party/LUFA/Drivers/USB/Core/StdRequestType.h \
+ third_party/LUFA/Drivers/USB/Core/StdDescriptors.h \
+ third_party/LUFA/Drivers/USB/Core/DeviceStandardReq.h \
+ third_party/LUFA/Drivers/USB/Core/Events.h \
+ third_party/LUFA/Drivers/USB/Core/StdDescriptors.h \
+ third_party/LUFA/Drivers/USB/Core/ConfigDescriptors.h \
+ third_party/LUFA/Drivers/USB/Core/HostStandardReq.h \
+ third_party/LUFA/Drivers/USB/Core/USBController.h \
+ third_party/LUFA/Drivers/USB/Core/USBInterrupt.h \
+ third_party/LUFA/Drivers/USB/Core/Device.h \
+ third_party/LUFA/Drivers/USB/Core/Endpoint.h \
+ third_party/LUFA/Drivers/USB/Core/DeviceStandardReq.h \
+ third_party/LUFA/Drivers/USB/Core/EndpointStream.h \
+ third_party/LUFA/Drivers/USB/Class/AndroidAccessoryClass.h \
+ third_party/LUFA/Drivers/USB/Class/../Core/USBMode.h \
+ third_party/LUFA/Drivers/USB/Class/AudioClass.h \
+ third_party/LUFA/Drivers/USB/Class/Device/AudioClassDevice.h \
+ third_party/LUFA/Drivers/USB/Class/Device/../../USB.h \
+ third_party/LUFA/Drivers/USB/Class/Device/../Common/AudioClassCommon.h \
+ third_party/LUFA/Drivers/USB/Class/Device/../Common/../../Core/StdDescriptors.h \
+ third_party/LUFA/Drivers/USB/Class/CDCClass.h \
+ third_party/LUFA/Drivers/USB/Class/Device/CDCClassDevice.h \
+ third_party/LUFA/Drivers/USB/Class/Device/../Common/CDCClassCommon.h \
+ third_party/LUFA/Drivers/USB/Class/HIDClass.h \
+ third_party/LUFA/Drivers/USB/Class/Device/HIDClassDevice.h \
+ third_party/LUFA/Drivers/USB/Class/Device/../Common/HIDClassCommon.h \
+ third_party/LUFA/Drivers/USB/Class/Device/../Common/HIDParser.h \
+ third_party/LUFA/Drivers/USB/Class/Device/../Common/../../../../Common/Common.h \
+ third_party/LUFA/Drivers/USB/Class/Device/../Common/HIDReportData.h \
+ third_party/LUFA/Drivers/USB/Class/Device/../Common/HIDClassCommon.h \
+ third_party/LUFA/Drivers/USB/Class/MassStorageClass.h \
+ third_party/LUFA/Drivers/USB/Class/Device/MassStorageClassDevice.h \
+ third_party/LUFA/Drivers/USB/Class/Device/../Common/MassStorageClassCommon.h \
+ third_party/LUFA/Drivers/USB/Class/MIDIClass.h \
+ third_party/LUFA/Drivers/USB/Class/Device/MIDIClassDevice.h \
+ third_party/LUFA/Drivers/USB/Class/Device/../Common/MIDIClassCommon.h \
+ third_party/LUFA/Drivers/USB/Class/Device/../Common/AudioClassCommon.h \
+ third_party/LUFA/Drivers/USB/Class/PrinterClass.h \
+ third_party/LUFA/Drivers/USB/Class/Device/PrinterClassDevice.h \
+ third_party/LUFA/Drivers/USB/Class/Device/../Common/PrinterClassCommon.h \
+ third_party/LUFA/Drivers/USB/Class/RNDISClass.h \
+ third_party/LUFA/Drivers/USB/Class/Device/RNDISClassDevice.h \
+ third_party/LUFA/Drivers/USB/Class/Device/../Common/RNDISClassCommon.h \
+ third_party/LUFA/Drivers/USB/Class/Device/../Common/CDCClassCommon.h \
+ third_party/LUFA/Drivers/USB/Class/StillImageClass.h \
+ lib/../config/appconfig.h lib/../lib/scsi.h lib/../lib/../storage.h \
+ lib/../lib/../descriptors.h lib/../lib/dataflashmanager.h \
+ config/appconfig.h lib/../lib/dataflashmanager.h lib/../descriptors.h \
+ third_party/LUFA/Common/Common.h \
+ third_party/LUFA/Drivers/Board/Dataflash.h \
+ third_party/LUFA/Drivers/Board/../../Common/Common.h lib/flash.h \
+ lib/uart.h
+
+lib/dataflashmanager.h:
+
+lib/../storage.h:
+
+lib/../descriptors.h:
+
+third_party/LUFA/Drivers/USB/USB.h:
+
+third_party/LUFA/Drivers/USB/../../Common/Common.h:
+
+third_party/LUFA/Drivers/USB/../../Common/Architectures.h:
+
+third_party/LUFA/Drivers/USB/../../Common/BoardTypes.h:
+
+third_party/LUFA/Drivers/USB/../../Common/ArchitectureSpecific.h:
+
+third_party/LUFA/Drivers/USB/../../Common/CompilerSpecific.h:
+
+third_party/LUFA/Drivers/USB/../../Common/Attributes.h:
+
+config/lufaconfig.h:
+
+third_party/LUFA/Drivers/USB/../../Common/Endianness.h:
+
+third_party/LUFA/Drivers/USB/Core/USBMode.h:
+
+third_party/LUFA/Drivers/USB/Core/../../../Common/Common.h:
+
+third_party/LUFA/Drivers/USB/Core/USBTask.h:
+
+third_party/LUFA/Drivers/USB/Core/USBMode.h:
+
+third_party/LUFA/Drivers/USB/Core/USBController.h:
+
+third_party/LUFA/Drivers/USB/Core/AVR8/USBController_AVR8.h:
+
+third_party/LUFA/Drivers/USB/Core/AVR8/../../../../Common/Common.h:
+
+third_party/LUFA/Drivers/USB/Core/AVR8/../USBMode.h:
+
+third_party/LUFA/Drivers/USB/Core/AVR8/../Events.h:
+
+third_party/LUFA/Drivers/USB/Core/AVR8/../../../../Common/Common.h:
+
+third_party/LUFA/Drivers/USB/Core/AVR8/../USBMode.h:
+
+third_party/LUFA/Drivers/USB/Core/AVR8/../USBTask.h:
+
+third_party/LUFA/Drivers/USB/Core/AVR8/../USBInterrupt.h:
+
+third_party/LUFA/Drivers/USB/Core/AVR8/../AVR8/USBInterrupt_AVR8.h:
+
+third_party/LUFA/Drivers/USB/Core/AVR8/../AVR8/../../../../Common/Common.h:
+
+third_party/LUFA/Drivers/USB/Core/AVR8/../AVR8/../USBMode.h:
+
+third_party/LUFA/Drivers/USB/Core/AVR8/../AVR8/../Events.h:
+
+third_party/LUFA/Drivers/USB/Core/AVR8/../AVR8/../USBController.h:
+
+third_party/LUFA/Drivers/USB/Core/AVR8/../Device.h:
+
+third_party/LUFA/Drivers/USB/Core/AVR8/../StdDescriptors.h:
+
+third_party/LUFA/Drivers/USB/Core/AVR8/../Events.h:
+
+third_party/LUFA/Drivers/USB/Core/AVR8/../USBInterrupt.h:
+
+third_party/LUFA/Drivers/USB/Core/AVR8/../Endpoint.h:
+
+third_party/LUFA/Drivers/USB/Core/AVR8/../AVR8/Endpoint_AVR8.h:
+
+third_party/LUFA/Drivers/USB/Core/AVR8/../AVR8/../USBTask.h:
+
+third_party/LUFA/Drivers/USB/Core/AVR8/../AVR8/../USBInterrupt.h:
+
+third_party/LUFA/Drivers/USB/Core/AVR8/../AVR8/Device_AVR8.h:
+
+third_party/LUFA/Drivers/USB/Core/AVR8/../AVR8/../StdDescriptors.h:
+
+third_party/LUFA/Drivers/USB/Core/AVR8/../AVR8/../Endpoint.h:
+
+third_party/LUFA/Drivers/USB/Core/AVR8/../Endpoint.h:
+
+third_party/LUFA/Drivers/USB/Core/AVR8/../DeviceStandardReq.h:
+
+third_party/LUFA/Drivers/USB/Core/AVR8/../StdRequestType.h:
+
+third_party/LUFA/Drivers/USB/Core/AVR8/../USBTask.h:
+
+third_party/LUFA/Drivers/USB/Core/AVR8/../USBController.h:
+
+third_party/LUFA/Drivers/USB/Core/AVR8/../EndpointStream.h:
+
+third_party/LUFA/Drivers/USB/Core/AVR8/../AVR8/EndpointStream_AVR8.h:
+
+third_party/LUFA/Drivers/USB/Core/Events.h:
+
+third_party/LUFA/Drivers/USB/Core/StdRequestType.h:
+
+third_party/LUFA/Drivers/USB/Core/StdDescriptors.h:
+
+third_party/LUFA/Drivers/USB/Core/DeviceStandardReq.h:
+
+third_party/LUFA/Drivers/USB/Core/Events.h:
+
+third_party/LUFA/Drivers/USB/Core/StdDescriptors.h:
+
+third_party/LUFA/Drivers/USB/Core/ConfigDescriptors.h:
+
+third_party/LUFA/Drivers/USB/Core/HostStandardReq.h:
+
+third_party/LUFA/Drivers/USB/Core/USBController.h:
+
+third_party/LUFA/Drivers/USB/Core/USBInterrupt.h:
+
+third_party/LUFA/Drivers/USB/Core/Device.h:
+
+third_party/LUFA/Drivers/USB/Core/Endpoint.h:
+
+third_party/LUFA/Drivers/USB/Core/DeviceStandardReq.h:
+
+third_party/LUFA/Drivers/USB/Core/EndpointStream.h:
+
+third_party/LUFA/Drivers/USB/Class/AndroidAccessoryClass.h:
+
+third_party/LUFA/Drivers/USB/Class/../Core/USBMode.h:
+
+third_party/LUFA/Drivers/USB/Class/AudioClass.h:
+
+third_party/LUFA/Drivers/USB/Class/Device/AudioClassDevice.h:
+
+third_party/LUFA/Drivers/USB/Class/Device/../../USB.h:
+
+third_party/LUFA/Drivers/USB/Class/Device/../Common/AudioClassCommon.h:
+
+third_party/LUFA/Drivers/USB/Class/Device/../Common/../../Core/StdDescriptors.h:
+
+third_party/LUFA/Drivers/USB/Class/CDCClass.h:
+
+third_party/LUFA/Drivers/USB/Class/Device/CDCClassDevice.h:
+
+third_party/LUFA/Drivers/USB/Class/Device/../Common/CDCClassCommon.h:
+
+third_party/LUFA/Drivers/USB/Class/HIDClass.h:
+
+third_party/LUFA/Drivers/USB/Class/Device/HIDClassDevice.h:
+
+third_party/LUFA/Drivers/USB/Class/Device/../Common/HIDClassCommon.h:
+
+third_party/LUFA/Drivers/USB/Class/Device/../Common/HIDParser.h:
+
+third_party/LUFA/Drivers/USB/Class/Device/../Common/../../../../Common/Common.h:
+
+third_party/LUFA/Drivers/USB/Class/Device/../Common/HIDReportData.h:
+
+third_party/LUFA/Drivers/USB/Class/Device/../Common/HIDClassCommon.h:
+
+third_party/LUFA/Drivers/USB/Class/MassStorageClass.h:
+
+third_party/LUFA/Drivers/USB/Class/Device/MassStorageClassDevice.h:
+
+third_party/LUFA/Drivers/USB/Class/Device/../Common/MassStorageClassCommon.h:
+
+third_party/LUFA/Drivers/USB/Class/MIDIClass.h:
+
+third_party/LUFA/Drivers/USB/Class/Device/MIDIClassDevice.h:
+
+third_party/LUFA/Drivers/USB/Class/Device/../Common/MIDIClassCommon.h:
+
+third_party/LUFA/Drivers/USB/Class/Device/../Common/AudioClassCommon.h:
+
+third_party/LUFA/Drivers/USB/Class/PrinterClass.h:
+
+third_party/LUFA/Drivers/USB/Class/Device/PrinterClassDevice.h:
+
+third_party/LUFA/Drivers/USB/Class/Device/../Common/PrinterClassCommon.h:
+
+third_party/LUFA/Drivers/USB/Class/RNDISClass.h:
+
+third_party/LUFA/Drivers/USB/Class/Device/RNDISClassDevice.h:
+
+third_party/LUFA/Drivers/USB/Class/Device/../Common/RNDISClassCommon.h:
+
+third_party/LUFA/Drivers/USB/Class/Device/../Common/CDCClassCommon.h:
+
+third_party/LUFA/Drivers/USB/Class/StillImageClass.h:
+
+lib/../config/appconfig.h:
+
+lib/../lib/scsi.h:
+
+lib/../lib/../storage.h:
+
+lib/../lib/../descriptors.h:
+
+lib/../lib/dataflashmanager.h:
+
+config/appconfig.h:
+
+lib/../lib/dataflashmanager.h:
+
+lib/../descriptors.h:
+
+third_party/LUFA/Common/Common.h:
+
+third_party/LUFA/Drivers/Board/Dataflash.h:
+
+third_party/LUFA/Drivers/Board/../../Common/Common.h:
+
+lib/flash.h:
+
+lib/uart.h:
